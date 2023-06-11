316b5e93f54aa1741a1b2fd15fe664ed5878da06
samuel carrizo <samuelcarrizot@gmail.com>
2023-06-11 19:24:54 -0300

    se agregaron componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,30 @@ import NavBar from './components/NavBar/NavBar';
 import Presentation from './components/Presentation/Presentation';
 import Search from './components/Search/Search';
 import Products from './components/Products/Products';
+import img from './Images/imgCentral.png';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
+  const Informed = () =>{
+    return (
+      <section className='container-informed'>
+      <h2>Don't miss out on the latest AI tools and updates! Sign up now to stay informed and ahead of the curve.</h2>
+      <section>
+        <input type='text' placeholder='Email'/>
+        <button>Join us!</button>
+      </section>
+      <div className='img'><img src={img} alt='img'/></div>
+      </section>
+    )
+  }
   return (
     <main className='App'>
       <NavBar />
       <Presentation/>
       <Search/>
       <Products/>
+      <Informed/>
+      <Footer/>
     </main>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <section className='title'>
+            <h1>The AI Helper</h1>
+            <div className='div-2'></div>
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+    </div>
+  )
+}
+
+export default Footer
